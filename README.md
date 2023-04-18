de2225b9ae34051740bc19caf8974c788af4ef63
Andres Fernandez <andres94.af@gmail.con>
2023-04-18 19:36:57 +0200

    creando proyecto base

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Spring Boot IG &copy;</h1>
+
+<h3><em>Diseñado en IDE Eclipse utilizando el lenguaje Java, SpringBoot, MySQL, Hibernate, Thymeleaf, Bootstrap</em></h3>
+
+<p># Funcionalidades: </p>
+
+<hr/>
+<p>Creador: Andres Mariano Fernández</p>
+<p>Año: 2023</p>
---
+++ src/main/java/com/instagram/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.instagram.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+	
+	@GetMapping("/")
+	public String inicio(Model model) {
+		model.addAttribute("titulo", "Hola desde Spring Boot IG");
+		return "usuario/HTML_BASE";
+	}
+
+}
