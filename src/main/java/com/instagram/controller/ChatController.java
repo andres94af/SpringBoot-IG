e6d1544703b12378ffde3a2cab9ab916094c6572
Andres Fernandez <andres94.af@gmail.con>
2023-05-12 14:38:26 +0200

    creado controlador chat y vista de chat

---
+++ src/main/java/com/instagram/controller/ChatController.java
@@ -0,0 +1,39 @@
+package com.instagram.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.instagram.model.Notificacion;
+import com.instagram.model.Usuario;
+import com.instagram.service.IUsuarioService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/direct")
+public class ChatController {
+
+	@Autowired
+	IUsuarioService usuarioService;
+
+	// REDIRECCION A LA VISTA MENSAJES DEL USUARIO LOGUEADO
+	@GetMapping("inbox")
+	public String inbox(Model model, HttpSession session) {
+		Optional<Usuario> usuarioLogueado = usuarioService.findById((Integer) session.getAttribute("idUsuario"));
+		List<Usuario> chats = usuarioService.findAllSeguidos(usuarioLogueado.get());
+		List<Notificacion> notificaciones = usuarioLogueado.get().getNotificaciones();
+		notificaciones.removeIf(n -> n.isRecibida());
+		model.addAttribute("title", "Bandeja de entrada  • Chats");
+		model.addAttribute("usuarioLogueado", usuarioLogueado.get());
+		model.addAttribute("notificaciones", notificaciones);
+		model.addAttribute("chats", chats);
+		return "usuario/mensajes";
+	}
+
+}
